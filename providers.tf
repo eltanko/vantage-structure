7eeb9441677524839a0d7152d5cd678f804b91a7
Darren Heath <dazheath@gmail.com>
2024-04-05 20:39:09 +0100

    modified:   .terraform.lock.hcl 	new file:   create-remote-storage.tf 	modified:   providers.tf

--- providers.tf
+++ providers.tf
@@ -3,9 +3,19 @@ terraform {
     vantage = {
       source = "vantage-sh/vantage"
     }
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">=3.0"
+      }
+    
+
   }
 }
 
 provider "vantage" {
   api_token = var.api_token
+}
+
+provider "azurerm" {
+    features {}
 }
